e836273f91955f361d64c04191c754043b768d94
priyanshisharma <priyanshi417@gmail.com>
2020-03-08 22:23:43 +0530

    added the blog

--- reddjaapp/admin.py
+++ reddjaapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from reddjaapp.models import Post
+from reddjaapp.models import Post, Comment
 
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Comment)
--- reddjaapp/models.py
+++ reddjaapp/models.py
@@ -23,3 +23,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('reddjaapp.Post',related_name = 'comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text

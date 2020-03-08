95a5ba10b6941832245a7b1822a5fe034819ac80
priyanshisharma <priyanshi417@gmail.com>
2020-03-09 01:52:43 +0530

    added postdetail view API

--- reddjaapp/api/urls.py
+++ reddjaapp/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.PostListAPIView.as_view(),name='post_list'),
    #path('about/',views.AboutView.as_view(),name='about'),
-    #path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>', views.PostDetailAPIView.as_view(), name='post_detail'),
     #path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     #path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     #path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
--- reddjaapp/api/views.py
+++ reddjaapp/api/views.py
@@ -1,7 +1,11 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView , RetrieveAPIView
 from reddjaapp.models import Post,Comment
 from .serializers import PostSerializer
 
 class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer

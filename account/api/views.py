ac401f60d893a69af74754ab1766b35ec5772c64
priyanshisharma <priyanshi417@gmail.com>
2020-03-26 19:56:32 +0530

    added registration view

--- account/api/views.py
+++ account/api/views.py
@@ -5,3 +5,14 @@ from rest_framework.decorators import api_view
 from account.api.serializers import RegistrationSerializer
 
 @api_view(['POST'])
+def registration_view(request):
+    serializer = RegistrationSerializer(data=request.data) 
+    data = {}
+    if serializer.is_valid():
+         account = serializer.save()
+         data["response"] = "Successfully registered!"
+         data["email"] = account.email
+         data["username"] = account.username
+    else:
+        data = serializer.errors
+    return Response(data)
